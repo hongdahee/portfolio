0c8333ca84e50c5e1596f7a19e5b3f373a14c6d1
hongdahee <heewhy3513@gmail.com>
2023-04-22 15:32:48 +0900

    fix: border 수정

--- src/components/common/IntroSkeleton/style.ts
+++ src/components/common/IntroSkeleton/style.ts
@@ -4,7 +4,7 @@ export const SkeletonWrapper = styled.div`
   .skeleton_intro {
     min-width: 190px;
     height: 270px;
-    border-radius: 4px;
+    border-radius: 50%;
     @media screen and (max-width: 79em) {
       min-width: 160px;
       height: 240px;
